4b8c94041114f955420fd10497586d96bdde3414
蕭澧邦 <shou692199@gmail.com>
2024-02-20 13:05:16 +0800

    Initial commit

---
+++ main.js
@@ -0,0 +1,39 @@
+const net = require('net');
+const { JSZhuyin } = require('jszhuyin');
+
+function getTranslatedString(bpmfInput) {
+    var result = "";
+    var jszhuyin = new JSZhuyin();
+
+    jszhuyin.load();
+    jszhuyin.oncandidateschange = function(candidates) {
+        console.log(candidates);
+        result = candidates[0][0];
+    };
+    jszhuyin.handleKey(bpmfInput);
+    jszhuyin.unload();
+
+    return result;
+}
+
+const server = net.createServer(function (connection) {
+    console.log('Client has connected.');
+    connection.on('end', function () {
+        console.log('Client closed connection.');
+    });
+
+    connection.on('data', function (data) {
+        console.log(data.toString());
+        connection.write('Response: ' + getTranslatedString(data.toString()));
+    });
+});
+
+let port = require('minimist')(process.argv.slice(2)).p;
+if(typeof port !== 'number') {
+    port = 3000;
+    console.warn("Port not specified or is invalid! Using default value: 3000.")
+}
+
+server.listen(port, function () {
+    console.log('Server listener started.');
+});
